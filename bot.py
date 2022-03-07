43798a6203eb0b37f074b80c3aeeefe385677ed9
mewmix <>
2022-03-06 20:19:47 -0800

    fix up bot

--- bot.py
+++ bot.py
@@ -4,69 +4,58 @@ import time
 
 from gnews import GNews
 
-import os
-search1 = 'Ukraine'
+search1 = "Ukraine"
 
-search2 = 'Russia'
-
-google_news = GNews(language='en', country='US', period='1h', max_results=5, exclude_websites=['yahoo.com', 'cnn.com'])
-news1 = google_news.get_news(search1)
-news2 =  google_news.get_news(search2)
-
-
-pretty_bn = json.dumps(news1, indent = 4)
-
-pretty_gn = json.dumps(news2, indent = 4)
-
-gn_len = len(pretty_gn)
-
-int_gn = int(gn_len)
-
-
-i = 5
-i1 = int_gn
-
-
-bn_len = len(pretty_bn)
-
-int_bn = int(bn_len)
-
-j = int_gn
-
-print(int_gn)
-
-json_dict1 = json.loads(pretty_gn)
-json_dict2 = json.loads(pretty_bn)
-
-
+search2 = "Hack" 
 
 while True:
 
+ google_news = GNews(language='en', country='US', period='1h', max_results=5, exclude_websites=['yahoo.com', 'cnn.com'])
+ news1 = google_news.get_news(search1)
+ news2 =  google_news.get_news(search2)
+ token = "" 
+ chat_id = ""
+ 
 
-  def send_msg(text):
-     token = ""
-     chat_id = ""
-     url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
-     results = requests.get(url_req)
-     print(results.json())
+ pretty_bn = json.dumps(news1, indent = 4)
 
-  send_msg(f"Here are the top 5 results for {search1}!")   
+ pretty_gn = json.dumps(news2, indent = 4)
 
-  for i in json_dict1:
+ json_dict1 = json.loads(pretty_bn)
+ json_dict2 = json.loads(pretty_gn)
 
 
-         send_msg(f"{i}")
-         time.sleep(5)    
 
-  send_msg(f"Here are the top 5 results for {search2}!")
 
-  for i in json_dict2:
 
-   slp = 1 
-   send_msg(f"{i}")
-   time.sleep(5)
+ def send_msg(text):
+    token 
+    chat_id 
+    url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
+    results = requests.get(url_req)
+    print(results.json())
 
-  break
-send_msg(f"Sleeping for {slp} hours")
-time.sleep(60 * 60 * slp)
+ send_msg(f"Here are the top 5 results for {search1}!")   
+
+ for i in json_dict1:
+
+
+        send_msg(f"{i}")
+        time.sleep(5)    
+
+ send_msg(f"Here are the top 5 results for {search2}!")
+
+ for i in json_dict2:
+
+  slp = 2
+  hour = 1
+  send_msg(f"{i}")
+  time.sleep(5)
+
+  
+ if slp > hour:
+  send_msg(f"Sleeping for {slp} hours")
+ else:
+  send_msg(f"Sleeping for {slp} hour")
+  time.sleep(60 * 60 * slp)
 
